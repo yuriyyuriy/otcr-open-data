7bcfdb4a48f3c47727520cb5e34ae35cec487dfd
Yuriy Tytla <drjamal90@live.com>
2014-10-22 13:35:42 -0500

    yeah

--- index.php
+++ index.php
@@ -1,45 +1,78 @@
+<?php
+function removeElem_PHP($divname) {
+  echo '<script type="text/javascript"> removeElement("'.$divname.'");</script>';
+}
+?>
 <html>
 	<head>
-		<style>
-			div{
-				background: grey;
-			}
-		 </style>
-
 		<script src="masonry.min.js" type="text/javascript"></script>
     	<link href="masonry.css" rel="stylesheet" type="text/css" />
-
+    	<link href="style.css" rel="stylesheet" type="text/css" />
+		<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    	<script type="text/javascript">
+    		$(window).load(function() {
+    			$('#container').masonry({
+        			columnWidth: '.box',
+        			itemSelector: '.box',
+        			isAnimated: true
+			    });
+			});
+    	</script>
+		<script type="text/javascript">
+		function removeElement(childDiv){
+			if (document.getElementById(childDiv)){   
+			  	var child = document.getElementById(childDiv);
+			  	child.parentNode.removeChild(child);
+				return true;
+			}
+			return false;
+		}
+		</script>
 
 		<title>PHP Test</title>
 	</head>
 	<body>
-		
-	   <p>You  may search either by first or last name</p> 
-	   <form  method="post" action="index.php?go"  id="searchform"> 
-	       <input  type="text" name="name"> 
-	       <input  type="submit" name="submit" value="Search"> 
-	   </form> 
-	   <div class="masonry js-masonry centered"  data-masonry-options='{ "columnWidth": ".grid-sizer", "itemSelector": ".item" , "gutter": 15 , "isFitWidth": true}'>
-	   <div class="grid-sizer">HIIII</div>
+
+		<div class = "topright" >
+			<form  method="post" action="index.php?go"  id="searchform"> 
+		    	<input type="text" name="name" size="45" placeholder="You may search either by first or last name">
+	       		<input type="submit" name="submit" value="" > 
+			</form> 
+		</div>
+	   <div class="masonry js-masonry centered " id="tile_container" data-masonry-options='{ "columnWidth": ".grid-sizer", "itemSelector": ".item" , "gutter": 15 , "isFitWidth": true}'>
+	   <div class="grid-sizer" id="grid_sizer"></div>
 <?php
-		$name='Yuriy'; 
+		$dom = new DOMDocument('1.0', 'iso-8859-1');
 		$con = mysqli_connect("localhost","root","");
 		$database="testdb";
 		mysqli_select_db($con,$database);
 		$result = mysqli_query($con,"SELECT * FROM testtable");
-		$i=1;
+		$total_divs=0;
+		$n="div_{$total_divs}_name"; 
+		$n_child= "html_child_{$total_divs}";
+		//echo "<p>".$div_0_name."</p>";
+		$attr_array= array( 'class' => 'item', 'width' => '200px', 'height' => '200px');
+		//$stream=streamWrapper::__construct();
 		if (!$result)
 		{
 			echo "Whaaat";
 		}
 		while($row=mysqli_fetch_array($result))
 		{
-			echo '<div class= "item" width="200px" height="200px" id= "item_$i">';
-			$FirstName  =$row['Name'];
-			echo '<p class="text">' .$FirstName . "</p>\n";
-			echo '</div>';
-		//	i++;
+				$FirstName  =$row['Name'];
+				$n="div_{$total_divs}_name";
+				${$n}= $dom->appendChild($dom->createElement('div'));
+				${$n_child}= ${$n}->appendChild($dom->createElement('p', $FirstName));
+				${$n_child}->setAttribute('class','text');
+				foreach ($attr_array as $key => $value) {
+       				$$n->setAttribute($key, $value);
+    			}
+				$$n->setAttribute('id', $n);
+				$total_divs= $total_divs+1;
 	  	}
+		$dom->formatOutput = true;
+		echo $dom->saveHTML();
+		$total_divs= $total_divs -1;
 
 		if(isset($_POST['submit']))
 		{
@@ -57,19 +90,33 @@
 	  				//-run  the query against the mysql query function1
 	 				$result=mysqli_query($con,$sql);	
 					//-create  while loop and loop through result set
-					$i=1;			
+					while($total_divs!=-1)
+					{
+						removeElem_PHP($n);
+						$dom->removeChild(${$n});
+						if ($total_divs==0)
+						{
+							break;
+						}
+						$total_divs=$total_divs -1;
+						$n="div_{$total_divs}_name";
+					}
+					//echo $dom->saveHTML();
+					//$total_divs=0;			
 					while($row=mysqli_fetch_array($result))
 					{
-						echo '<div class= "item" width="200px" height="200px" id= "item_$i">';
 						$FirstName  =$row['Name'];
-						echo '<p class="text">' .$FirstName . "</p>\n";
-						echo '</div>';
-					//	i++;
+						$n="div_{$total_divs}_name";
+						${$n}= $dom->appendChild($dom->createElement('div'));
+						${$n_child}= ${$n}->appendChild($dom->createElement('p', $FirstName));
+						${$n_child}->setAttribute('class','text');
+						foreach ($attr_array as $key => $value) {
+			   				$$n->setAttribute($key, $value);
+						}
+						$$n->setAttribute('id', $n);
+						$total_divs= $total_divs+1;
 				  	}
-				}
-				else
-				{
-				  echo  "<p>Please enter a search query</p>";
+					echo $dom->saveHTML();
 				}
 			}
 		}
@@ -77,4 +124,3 @@
 </div>
 </body>
 </html>
-
